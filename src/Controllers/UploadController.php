e8aa823ae4e545f1d2b14cb86a4e8f3b456b6899
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-14 17:27:32 +0300

    add UploadController

---
+++ src/Controllers/UploadController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ozerich\FileStorage\Controllers;
+
+use Illuminate\Routing\Controller;
+use Ozerich\FileStorage\Models\File;
+use Ozerich\FileStorage\Storage;
+
+class UploadController extends Controller
+{
+    /**
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index(Storage $storage)
+    {
+        /** @var File $file */
+        $file = $storage->createFromRequest();
+
+        if (!$file) {
+            abort(400, 'Error upload file');
+        }
+
+        return $file->getUploadResponseJson();
+    }
+}
