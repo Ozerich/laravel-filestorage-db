b4f4c883a7412b751eb0d517825d17882855a23a
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-17 13:50:14 +0300

    fix migrations

---
+++ migrations/2020_06_15_141806_files_change_types.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FilesChangeTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->unsignedInteger('width')->nullable()->change();
+            $table->unsignedInteger('height')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
